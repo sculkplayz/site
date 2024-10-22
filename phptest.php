0c337a9ff588ba6877d1cb3767efea1d42741a7a
sculkplayz <korochedlyaroblox@gmail.com>
2024-10-22 16:30:49 +0500

    Create phptest.php

---
+++ phptest.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <title>sculk playz</title>
+
+        <!-- Load external CSS styles -->
+
+    </head>
+
+    <body>
+
+        <h1>SCULK PLAYZ</h1>
+        <div class="contentDiv">
+            <label>Привет, зритель! </label>
+            <label>Это я sculk playz! </label>
+        </div>
+    </body>
+
+</html>
